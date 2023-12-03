50c9b12c2bbe6cb7566c6694b80f1db7026df924
hussamQassem <hussamcacem@gmail.com>
2023-12-03 06:20:18 +0000

    creat methods with properties
    car,car0 methods
    make, model , color, engin size, mileage , price

--- src/main/java/com/mycompany/carsale/vehicleBrand.java
+++ src/main/java/com/mycompany/carsale/vehicleBrand.java
@@ -9,5 +9,28 @@ package com.mycompany.carsale;
  * @author hussa
  */
 public class vehicleBrand {
+     String make;
+    String model;
+    String color;
+    double enginSize;
+    int mileage;
+    int price;
+
+    public vehicleBrand(double enginSize, int mileagge, int price) {
+        this.enginSize = enginSize;
+        this.mileage = mileage;
+        this.price = price;
+    }
+    public vehicleBrand(String make,String model,String color){
+    this.make=make;
+    this.model=model;
+    this.color=color;
     
+    }
+    public void car(String make,String model,String color){
+         System.out.println("the "+make+" "+model+" with color "+color);
+    }
+    public void car0(double enginSize, int mileage, int price){
+        System.out.println("has size "+enginSize+"  mileage "+mileage+" with price of "+price);
+    } 
 }
